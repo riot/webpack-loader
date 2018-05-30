79935fb9b3214895a80ee8329edf58787ab4f6f0
Tõnis Bender <tonis@dolmit.com>
2018-05-30 17:17:53 +0300

    Allow disabling sourcemaps from loader config

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ module.exports = function(source) {
   // compile and generate sourcemaps
   const {code, map} = compile(
     source,
-    Object.assign(opts, { sourcemap: this.sourceMap }),
+    Object.assign(opts, { sourcemap: opts.sourcemap != null ? opts.sourcemap : this.sourceMap }),
     this.resourcePath
   )
 
